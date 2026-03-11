209528818f82ad2fcf523bfc678f8660f808a88d
pppss102 <73379683+pppss102@users.noreply.github.com>
2026-03-11 21:32:36 +0200

    fix formatting stuff

--- src/wallet-backend/app/app.py
+++ src/wallet-backend/app/app.py
@@ -10,27 +10,32 @@ app.config["QUART_AUTH_MODE"] = "bearer"
 app.secret_key = secrets.token_urlsafe(16)
 auth = QuartAuth(app)
 
+
 @app.before_serving
 async def startup():
     app.db_engine = await db.init_db()
     app.blob_dir = "blob"
     os.mkdir(app.blob_dir)
 
+
 @app.after_serving
 async def shutdown():
     await app.db_engine.dispose()
 
+
 def is_user_request_valid(req):
-    return req is not None \
-        and "username" in req \
-        and "password" in req \
-        and isinstance(req["username"], str) \
+    return (
+        req is not None
+        and "username" in req
+        and "password" in req
+        and isinstance(req["username"], str)
         and isinstance(req["password"], str)
+    )
+
 
 def is_blob_request_valid(req):
-    return req is not None \
-        and "blob" in req \
-        and isinstance(req["blob"], str)
+    return req is not None and "blob" in req and isinstance(req["blob"], str)
+
 
 @app.route("/api/register", methods=["POST"])
 async def register():
@@ -48,6 +53,7 @@ async def register():
         return jsonify({"success": False, "status": "Failed to create user"}), 400
 
     return jsonify({"success": True, "Status": "User created successfully"})
+
 
 @app.route("/api/login", methods=["POST"])
 async def login():
@@ -72,16 +78,18 @@ async def login():
     token = auth.dump_token(user["username"])
     return jsonify({"token": token})
 
+
 @app.route("/api/get_blob", methods=["GET"])
 @login_required
 async def get_blob():
     username = current_user.auth_id
-    
+
     try:
         blob = await db.get_blob(current_app.db_engine, username, current_app.blob_dir)
         return jsonify({"blob": blob})
     except Exception:
         return Response(status=500)
+
 
 @app.route("/api/store_blob", methods=["POST"])
 @login_required
@@ -93,10 +101,14 @@ async def store_blob():
         return jsonify({"success": False, "status": "Invalid input JSON"}), 400
 
     try:
-        await db.write_blob(current_app.db_engine, username, current_app.blob_dir, req["blob"])
+        await db.write_blob(
+            current_app.db_engine, username, current_app.blob_dir, req["blob"]
+        )
+
         return jsonify({"success": True, "status": "Blob stored successfully"})
     except Exception:
         return jsonify({"success": False, "status": "Failed to store blob"}), 500
+
 
 if __name__ == "__main__":
     app.run()
